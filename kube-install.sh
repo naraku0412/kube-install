7fa9b6a6a02502c348e28f09209a9f06c4968c6e
humstarman <lueyoung7@gmail.com>
2018-04-17 15:52:15 +0800

    init

--- kube-install.sh
+++ kube-install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-
+#curl -s https://raw.githubusercontent.com/humstarman/kube-install/master/test.sh | /bin/bash
+if [ "init" == "$1" ]; then
+  curl -s https://raw.githubusercontent.com/humstarman/kube-install/master/test.sh | /bin/bash
+elif [ "join" == "$1" ]; then
+  echo 1
+else
+  echo " - Usage:"
+  echo "use 'init' to deploy a master;"
+  echo "use 'join' to deploy a node."
+fi
